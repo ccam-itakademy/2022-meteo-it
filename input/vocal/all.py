de2b2b8fcfe093fc3da8200ee5c559982f3afba8
ccam-itakademy <c.cam@it-students.fr>
2022-06-23 16:00:18 +0200

    audio input raspberry

--- input/vocal/all.py
+++ input/vocal/all.py
@@ -1,46 +1,46 @@
 import pyaudio
 import wave
 import speech_recognition as sr
+import py_error_handling
 
-
-CHUNK = 1024
+CHUNK = 4*1024
 FORMAT = pyaudio.paInt16
-CHANNELS = 2
+CHANNELS = 1
 RATE = 44100
 RECORD_SECONDS = 3
-MIC_INDEX = 1 # Index à modifier en fonction de l'index du micro
+MIC_INDEX = 2 # Index à modifier en fonction de l'index du micro
 WAVE_OUTPUT_FILENAME = "record.wav"
 
-p = pyaudio.PyAudio()
+with py_error_handling.noalsaerr():
+    p = pyaudio.PyAudio()
 
-stream = p.open(format=FORMAT,
+    stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
                 input=True,
                 input_device_index=MIC_INDEX,
                 frames_per_buffer=CHUNK)
 
-print("* Enregistrement de la voix")
+    print("* Enregistrement de la voix")
 
-frames = []
+    frames = []
 
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK, exception_on_overflow = False)
+        frames.append(data)
 
-print("* Enregistrement finis")
+    print("* Enregistrement fini")
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
 
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
-
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
 
 r = sr.Recognizer()
 with sr.AudioFile('record.wav') as source:
@@ -48,7 +48,7 @@ with sr.AudioFile('record.wav') as source:
     try:
         # using google speech recognition
         text = r.recognize_google(audio_text, language="fr-FR")
-        print('Convesrion du fichier audio en text...')
+        print('Conversion du fichier audio en text...')
         print(text)
         fichier = open("city.txt", "w")
         fichier.write(text)
