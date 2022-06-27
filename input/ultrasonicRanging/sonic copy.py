d4ca9af3476f30544c5a4650f3440534ad0c0c04
admin <admin@VIL-S02-iMac-ITA-15.local>
2022-06-27 11:39:51 +0200

    add info md

---
+++ input/ultrasonicRanging/sonic copy.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+########################################################################
+# Filename    : UltrasonicRanging.py
+# Description : Get distance via UltrasonicRanging sensor
+# auther      : www.freenove.com
+# modification: 2019/12/28
+########################################################################
+import RPi.GPIO as GPIO
+import time
+
+trigPin = 16
+echoPin = 18
+MAX_DISTANCE = 60
+
+def get0or1()
